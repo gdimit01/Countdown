a306fe6d313408f0902df0dc764609876745fb04
Georgi Dimitrov <gd353@kent.ac.uk>
2023-04-23 09:38:36 +0100

    made the confetti conditional

--- script.js
+++ script.js
@@ -1,59 +1,66 @@
-const startNumberInput = document.getElementById("startNumber");
-const startButton = document.getElementById("start");
-const pauseButton = document.getElementById("pause");
-const replayButton = document.getElementById("replay");
-const inSpan = document.querySelector(".in");
-const finalMessage = document.querySelector(".final");
+window.onload = function () {
+  const startNumberInput = document.getElementById("startNumber");
+  const startButton = document.getElementById("start");
+  const pauseButton = document.getElementById("pause");
+  const replayButton = document.getElementById("replay");
+  const inSpan = document.querySelector(".in");
+  const finalMessage = document.querySelector(".final");
 
-let countdown;
-let currentNumber;
-let isPaused = false;
+  let countdown;
+  let currentNumber;
+  let isPaused = false;
 
-function startCountdown() {
-  const timeWarning = document.getElementById("timeWarning"); // Get the warning message element
+  function startCountdown() {
+    const timeWarning = document.getElementById("timeWarning");
 
-  currentNumber = parseInt(startNumberInput.value);
-  inSpan.textContent = currentNumber;
-  countdown = setInterval(() => {
-    if (!isPaused) {
-      if (currentNumber <= 0) {
-        clearInterval(countdown);
-        finalMessage.style.display = "block";
-        startButton.disabled = false;
-      } else {
-        currentNumber--;
-        inSpan.textContent = currentNumber;
-        inSpan.animate([{ opacity: 1 }, { opacity: 0 }], {
-          duration: 1000,
-          fill: "forwards",
-        });
+    currentNumber = parseInt(startNumberInput.value);
+    inSpan.textContent = currentNumber;
+    countdown = setInterval(() => {
+      if (!isPaused) {
+        if (currentNumber <= 0) {
+          clearInterval(countdown);
+          finalMessage.style.display = "block";
+          startButton.disabled = false;
 
-        // Show or hide the warning message based on the countdown value
-        if (currentNumber === 30) {
-          timeWarning.style.display = "block";
-        } else if (currentNumber === 29) {
-          timeWarning.style.display = "none";
+          // Trigger the confetti animation
+          confetti({
+            particleCount: 100,
+            spread: 70,
+            origin: { y: 0.6 },
+          });
+        } else {
+          currentNumber--;
+          inSpan.textContent = currentNumber;
+          inSpan.animate([{ opacity: 1 }, { opacity: 0 }], {
+            duration: 1000,
+            fill: "forwards",
+          });
+
+          if (currentNumber === 30) {
+            timeWarning.style.display = "block";
+          } else if (currentNumber === 29) {
+            timeWarning.style.display = "none";
+          }
         }
       }
-    }
-  }, 1000);
-  startButton.disabled = true;
-}
+    }, 1000);
+    startButton.disabled = true;
+  }
 
+  function pauseCountdown() {
+    isPaused = !isPaused;
+    pauseButton.textContent = isPaused ? "Resume" : "Pause";
+  }
 
-function pauseCountdown() {
-  isPaused = !isPaused;
-  pauseButton.textContent = isPaused ? "Resume" : "Pause";
-}
+  function resetCountdown() {
+    clearInterval(countdown);
+    isPaused = false;
+    pauseButton.textContent = "Pause";
+    startButton.disabled = false;
+    startCountdown();
+  }
 
-function resetCountdown() {
-  clearInterval(countdown);
-  isPaused = false;
-  pauseButton.textContent = "Pause";
-  startButton.disabled = false; // Enable the start button when the countdown is reset
-  startCountdown();
-}
-
-startButton.addEventListener("click", startCountdown);
-pauseButton.addEventListener("click", pauseCountdown);
-replayButton.addEventListener("click", resetCountdown);
+  startButton.addEventListener("click", startCountdown);
+  pauseButton.addEventListener("click", pauseCountdown);
+  replayButton.addEventListener("click", resetCountdown);
+};
